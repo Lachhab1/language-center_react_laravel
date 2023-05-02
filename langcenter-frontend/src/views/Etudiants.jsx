1c32640e4d445e7b5fc9351266f8d397aff7f406
youssef <youssefhafiane82@gmail.com>
2023-05-02 13:27:05 +0100

    Button ajouter et table

--- langcenter-frontend/src/views/Etudiants.jsx
+++ langcenter-frontend/src/views/Etudiants.jsx
@@ -1,11 +1,18 @@
+import React, { useState } from 'react';
 export default function Etudiants(){
+    
+    const [students, setStudents] = useState([]);
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    
     return(
         <div className="Container">
             <div className="row">
                 <div className="col-md-2">
-                    <h1>Etudiants Page </h1>
-                    <h2>Students</h2>
-                    <p>Home &gt;  Students</p>
+                   
+                    <h2>Etudiants</h2>
+                    <p>Home &gt;  Etudiants</p>
                 </div>
             </div>
             <div className="Container bg-light">
@@ -16,19 +23,33 @@ export default function Etudiants(){
                 </div>
                 
                 <div className="row">
-                    <div className="col-5">
-                        <form action="" >
+                    <div className="col-3">
+                        <form>
                             <input className="form-control" type="text" name="Nom" id="1" placeholder="Chercher avec le nom" />
                         </form>
                     </div>
-                    <div className="col-5">
-                        <form action="" >
+                    <div className="col-3">
+                        <form>
                             <input className="form-control" type="text" name="Class" id="2" placeholder="Chercher avec la class" />
                         </form>
                     </div>
-                    <div className="col">
+                    <div className="col-3">
                         <button className="btn btn-danger"> Chercher </button>
                     </div>
+                    <div className="col-3">
+                        <button className="btn btn-success"> Ajouter </button>
+                    </div>
+                </div>
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nom</th>
+                                <th>Prenom</th>
+                            </tr>
+                        </thead>
+                    </table>
                 </div>
                 </div>
             </div>
