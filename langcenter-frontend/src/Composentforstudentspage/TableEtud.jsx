5fefbc101d7fe185b2a2332a4bdb28b534f543d9
youssef <youssefhafiane82@gmail.com>
2023-05-02 14:53:39 +0100

    Faire un composant pour page etudiant qui affcihe un tableau des etudiants

---
+++ langcenter-frontend/src/Composentforstudentspage/TableEtud.jsx
@@ -0,0 +1,31 @@
+export default function TableEtud()
+{
+
+    return(
+    <div>
+                    <table className='table'>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Gender</th>
+                                <th>Class</th>
+                                <th>Parents</th>
+                                <th>Phone</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                             
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+    )
+
+}
--- langcenter-frontend/src/views/Etudiants.jsx
+++ langcenter-frontend/src/views/Etudiants.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import TableEtud from '../Composentforstudentspage/TableEtud';
 
 export default function Etudiants(){
     
@@ -40,30 +40,7 @@ export default function Etudiants(){
                     </div>
                 </div>
                 <br />
-                <div>
-                    <table className='table'>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Name</th>
-                                <th>Gender</th>
-                                <th>Class</th>
-                                <th>Parents</th>
-                                <th>Phone</th>
-                                <th>Status</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                             
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+                <TableEtud/>
                 </div>
             </div>
                 
