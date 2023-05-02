bf496bfcf06a21f1b465ad7d52cd1cc5472fecd2
youssef <youssefhafiane82@gmail.com>
2023-05-02 13:49:15 +0100

    Donner th de tableau

--- langcenter-frontend/src/views/Etudiants.jsx
+++ langcenter-frontend/src/views/Etudiants.jsx
@@ -1,55 +1,67 @@
 import React, { useState } from 'react';
+
+
 export default function Etudiants(){
     
-    const [students, setStudents] = useState([]);
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
+    
     
     return(
         <div className="Container">
             <div className="row">
                 <div className="col-md-2">
                    
-                    <h2>Etudiants</h2>
-                    <p>Home &gt;  Etudiants</p>
+                    <h2>Students</h2>
+                    <p>Home &gt;  Students</p>
                 </div>
             </div>
             <div className="Container bg-light">
                 <div className="row">
-                    <div className="col-4">
-                        <h2>Tous les etudiants</h2> 
+                    <div className="col">
+                        <h2>All Students</h2> 
                     </div>
                 </div>
-                
+                <br />
                 <div className="row">
                     <div className="col-3">
                         <form>
-                            <input className="form-control" type="text" name="Nom" id="1" placeholder="Chercher avec le nom" />
+                            <input className="form-control" type="text" name="Nom" id="1" placeholder="Search by name" />
                         </form>
                     </div>
                     <div className="col-3">
                         <form>
-                            <input className="form-control" type="text" name="Class" id="2" placeholder="Chercher avec la class" />
+                            <input className="form-control" type="text" name="Class" id="2" placeholder="Search by class" />
                         </form>
                     </div>
                     <div className="col-3">
-                        <button className="btn btn-danger"> Chercher </button>
+                        <button className="btn btn-danger"> Search </button>
                     </div>
                     <div className="col-3">
-                        <button className="btn btn-success"> Ajouter </button>
+                        <button className="btn btn-success"> Add Student </button>
                     </div>
                 </div>
+                <br />
                 <div>
-                    <table>
+                    <table className='table'>
                         <thead>
                             <tr>
                                 <th>ID</th>
-                                <th>Nom</th>
-                                <th>Prenom</th>
+                                <th>Name</th>
+                                <th>Gender</th>
+                                <th>Class</th>
+                                <th>Parents</th>
+                                <th>Phone</th>
+                                <th>Status</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                             
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
                 </div>
