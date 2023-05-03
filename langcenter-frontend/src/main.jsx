276eb6388c11a668f889859c49d3022e9cca275d
unknown <aymankacemi01@gmail.com>
2023-05-03 15:30:10 +0100

    avatar
     changer photo de profile

--- langcenter-frontend/src/main.jsx
+++ langcenter-frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/js/bootstrap.min.js'
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 import "./app.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
