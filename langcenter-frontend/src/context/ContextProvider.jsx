74c26b3005e54f4c368690442e101a0b57fd7b9d
Lachhab1 <Mohamed172lachhab@gmail.com>
2023-05-11 09:17:55 +0000

    context provider for backend auth

--- langcenter-frontend/src/context/ContextProvider.jsx
+++ langcenter-frontend/src/context/ContextProvider.jsx
@@ -1,30 +1,55 @@
-import { useContext, useState, useTransition } from "react";
+import axios from "../api/axios";
+import { useContext, useState } from "react";
 import {createContext} from "react";
-
+import { useEffect } from "react";
 const stateContext = createContext({
     user: null,
-    token: null,
     setUser: () => {},
-    setToken: () => {}
+    setToken: () => {},
+    getUser: () => {},
+    login: () => {},
+    logout: () => {}
 })
 export const ContextProvider = ({children}) => {
-    const [user,setUser] = useState({});
-    const [token,_setToken] = useState(localStorage.getItem('ACCES_TOKEN'));
+    const [user,setUser] = useState(null);
+    const [errors,setErrors] = useState([]);
+    const csrf = () => axios.get("/sanctum/csrf-cookie");
 
-    const setToken = (token) => {
-        _setToken(token)
-        if(token){
-            localStorage.setItem('ACCES_TOKEN',token)
-        } else{
-            localStorage.removeItem('ACCES_TOKEN')
+                const getUser = async () => {
+                    try {
+                            const data = await axios.get('/api/user');
+                            setUser(data.data);
+
+                    } catch (error) {
+                        console.log(error);
+                    }
+                }
+                const login = async ({...data}) => {
+                    await csrf();
+                    try {
+                        await axios.post('/api/login',data);
+                        await getUser();
+        } catch (error) {
+            if(error.response.status === 422)
+            {
+                setErrors(error.response.data.errors);
+            }
         }
+        
+    }
+    const logout = () => {
+        axios.post('/api/logout').then(() => {
+            setUser(null);
+        })
+        
     }
         return(
-        <stateContext.Provider value={{
+            <stateContext.Provider value={{
             user,
-            token,
             setUser,
-            setToken
+            getUser,
+            login,
+            logout,
         }}>
             {children}
         </stateContext.Provider>
