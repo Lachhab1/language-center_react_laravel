96d07254c54a95275d63e0aaa11b411526d17d5c
Lachhab1 <Mohamed172lachhab@gmail.com>
2023-05-15 20:54:52 +0100

    seeder

--- langcenter-backend/database/seeders/DatabaseSeeder.php
+++ langcenter-backend/database/seeders/DatabaseSeeder.php
@@ -12,11 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::create(['email' => 'admin@admin.com','password' => bcrypt('12345678'),'first_name' => 'admin','last_name' => 'admin','role' => 'admin','cin' => 'bv1895', 'birthday' => '2002-01-01', 'date_of_hiring' => '2022-01-01' ,'gender' => 'male','phone' => '0687439035','address' => 'qooqo','image' => 'empty']);
+
     }
 }
