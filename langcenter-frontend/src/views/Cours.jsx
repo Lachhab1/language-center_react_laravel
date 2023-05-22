3f414be4867438025b55179bfce1739a44f71726
youssef <youssefhafiane82@gmail.com>
2023-05-22 16:33:09 +0100

    creer sidebarsec

--- langcenter-frontend/src/views/Cours.jsx
+++ langcenter-frontend/src/views/Cours.jsx
@@ -1,10 +1,13 @@
 import TableCourse from "../components/Composantsforcoursepage/TableCourse"
+import SidebarSec from "../components/ConponentsidebarSec/SidebarSec"
 
 
 export default function Cours(){
     return(
         <div>
             <TableCourse/>
+            
         </div>
+        
     )
 }
