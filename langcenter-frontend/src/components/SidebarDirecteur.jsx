58260fa97419aa08263b72c7bd81b4a75c390442
unknown <aymankacemi01@gmail.com>
2023-05-22 22:31:06 +0100

    directeur role

---
+++ langcenter-frontend/src/components/SidebarDirecteur.jsx
@@ -0,0 +1,106 @@
+import { Nav } from 'react-bootstrap';
+import { Link, NavLink } from "react-router-dom";
+
+import { Bars3BottomLeftIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import centreLogo from "../images/EnglishCastle_HQ.png";
+import cours from "../images/icons/cours.svg";
+import dashboard from "../images/icons/dashboard.svg";
+import empTemps from "../images/icons/empTemps.svg";
+import enseignant from "../images/icons/enseignant.svg";
+import etudiant from "../images/icons/etudiant.svg";
+import groupes from "../images/icons/groupes.svg";
+import disco from "../images/icons/logout.svg";
+import paiements from "../images/icons/paiements.svg";
+import parents from "../images/icons/parents.svg";
+import presence from "../images/icons/presence.svg";
+import results from "../images/icons/results.svg";
+import salles from "../images/icons/salles.svg";
+import { UseStateContext } from '../context/ContextProvider';
+
+
+export default function SidebarDirecteur() {
+    const { logout } = UseStateContext();
+    const [openSidebar, setOpenSidebar] = useState(false);
+    const handleOpen = () => {
+        setOpenSidebar((prev) => !prev);
+    }
+    const handleLocalStorage = () => {
+        localStorage.removeItem('ACCES_TOKEN');
+        window.location.href = '/';
+    }
+    return (
+        <div style={{ width: openSidebar ? "20%" : "auto" }} className="">
+
+            <div className={` sidebar-container`} style={{ backgroundColor: "#242B5E" }} >
+                <div className={`logo-container m-0 d-flex flex-row-reverse justify-content-between align-items-center`} >
+                    <div className={openSidebar ? "w-25" : "w-100"}>
+                        <button onClick={handleOpen} className="OpenCloseBtn2">{openSidebar ? <XMarkIcon width={25} className="text-white" style={{ color: "white" }} /> : <Bars3BottomLeftIcon width={40} className="text-white" />}</button>
+                    </div>
+                    <div className="ms-4">
+                        {openSidebar && <Link className="" to="/dashboardDirecteur"><img src={centreLogo} className="logo"></img></Link>}
+                    </div>
+                </div>
+
+                <div className="Sidebar h-100 fs-6">
+                    <Nav className="flex-column" defaultActiveKey="/dashboardDirecteur" >
+                        <Nav.Item><NavLink className="a nav-link link-light" to="/dashboardDirecteur"><img src={dashboard} />{openSidebar && "Dashboard"}</NavLink></Nav.Item>
+                        <Nav.Item><NavLink className="a nav-link link-light" to="/course"><img src={cours} />{openSidebar && "Course"}</NavLink></Nav.Item>
+                        <Nav.Item><NavLink className="a nav-link link-light" to="/teacher"><img src={enseignant} />{openSidebar && "Teacher"}</NavLink></Nav.Item>
+                        <Nav.Item><NavLink className="a nav-link link-light" to="/classroom"><img src={salles} />{openSidebar && "Classroom"}</NavLink></Nav.Item>
+                        <Nav.Item><NavLink className="a nav-link link-light" to="/schedule"><img src={empTemps} />{openSidebar && "Schedule"}</NavLink></Nav.Item>
+                       
+                        {/* Paiements: le span pour cacher ces elements a fin de remplacer le collapse avec dropdown dans les petits ecrans */}
+                        {openSidebar ?
+                            <Nav.Item className="nav-item link-light"><a className="a nav-link link-light  dropdown-toggle" href="#dropFees" id="menu" data-bs-toggle="collapse"><img src={paiements} />{openSidebar && "Paiement"}</a>
+                                <ul className="collapse
+                        " id="dropFees" data-bs-parent="#menu" >
+                                    <Nav.Item ><NavLink className="a nav-link link-light" to="/fees/teacher">Teacher fees </NavLink></Nav.Item>
+                                    <Nav.Item ><NavLink className="a nav-link link-light" to="/fees/student">Student fees</NavLink></Nav.Item>
+                                </ul>
+                            </Nav.Item>
+                            :
+                            <Nav.Item className="nav-item"><a onClick={handleOpen} className="a nav-link link-light dropdown-toggle " href="#dropFees" id="menu" data-bs-toggle="collapse"><img src={paiements} /></a>
+                            </Nav.Item>
+                        }
+
+    
+
+                        <Nav.Item><NavLink className="a nav-link link-light" to="/student"><img src={etudiant} />{openSidebar && "Student"}</NavLink></Nav.Item>
+                        <Nav.Item><NavLink className="a nav-link link-light" to="/parent"><img src={parents} />{openSidebar && "Parent"}</NavLink></Nav.Item>
+                        <Nav.Item><NavLink className="a nav-link link-light" to="/class"><img src={groupes} />{openSidebar && "Groupe"}</NavLink></Nav.Item>
+
+                        {/* Presences le span pour cacher ces elements a fin de remplacer le collapse avec dropdown dans les petits ecrans */}
+
+                        {openSidebar ?
+                            <Nav.Item className="nav-item link-light"><a className="a nav-link link-light  dropdown-toggle" href="#dropPresence" id="menu" data-bs-toggle="collapse"><img src={presence} />{openSidebar && "Attendance"}</a>
+                                <ul className="collapse " id="dropPresence" data-bs-parent="#menu" >
+                                    <Nav.Item ><NavLink className="a nav-link link-light" to="/attendance/student">Student</NavLink></Nav.Item>
+                                    <Nav.Item ><NavLink className="a nav-link link-light" to="/attendance/teacher">Teacher </NavLink></Nav.Item>
+                                </ul>
+                            </Nav.Item>
+                            :
+                            <Nav.Item className="nav-item"><a onClick={handleOpen} className="a nav-link link-light dropdown-toggle " href="#dropFees" id="menu" data-bs-toggle="collapse"><img src={presence} /></a>
+                            </Nav.Item>
+                        }
+
+
+
+
+
+
+                        <Nav.Link className='a nav-link link-light' onClick={logout}><img src={disco} />{openSidebar && "Logout"}</Nav.Link>
+
+                    </Nav>
+                </div>
+
+            </div>
+
+
+
+        </div>
+    )
+
+}
+
+
---
+++ langcenter-frontend/src/views/DashboardDirecteur.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import Card from '../components/DashboardCardsCompo/Card';
+import student from '../images/icons/student4dash.png';
+import teacher from '../images/icons/teacher4dash.png';
+import money from '../images/icons/money4dash.png';
+import parents from '../images/icons/parent4dash.png';
+import DoughnutChart from '../components/DashboardCardsCompo/DoughnutChart';
+import LineChart from '../components/DashboardCardsCompo/LineChart';
+import BarChart from '../components/DashboardCardsCompo/BarChart';
+import TimetableScheduler from '../components/DashboardCardsCompo/TimetableScheduler';
+import '../components/DashboardCardsCompo/charts.css';
+
+const DashboardDirecteur = () => {
+  const maleCount = 30;
+  const femaleCount = 20;
+
+  const [chartData, setChartData] = useState({
+    courses: [
+      { course: 'English', students: 45 },
+      { course: 'French', students: 60 },
+      { course: 'Italian', students: 30 },
+      { course: 'Spanish', students: 55 },
+      { course: 'Arabic', students: 50 },
+    ],
+  });
+
+
+
+  const timetableData = [
+    { groupName: 'Group 1', courseName: 'English', salle: 'A101', dateStart: new Date(2023, 4, 1, 9, 0), dateEnd: new Date(2023, 4, 1, 10, 30) },
+    { groupName: 'Group 2', courseName: 'French', salle: 'B202', dateStart: new Date(2023, 4, 1, 11, 0), dateEnd: new Date(2023, 4, 1, 12, 30) },
+    { groupName: 'Group 3', courseName: 'Spanish', salle: 'C303', dateStart: new Date(2023, 4, 1, 13, 0), dateEnd: new Date(2023, 4, 1, 14, 30) },
+    { groupName: 'Group 1', courseName: 'English', salle: 'A101', dateStart: new Date(2023, 4, 6, 9, 0), dateEnd: new Date(2023, 4, 6, 10, 30) },
+    { groupName: 'Group 2', courseName: 'French', salle: 'B202', dateStart: new Date(2023, 4, 6, 11, 0), dateEnd: new Date(2023, 4, 6, 12, 30) },
+    { groupName: 'Group 3', courseName: 'Spanish', salle: 'C303', dateStart: new Date(2023, 4, 6, 13, 0), dateEnd: new Date(2023, 4, 6, 14, 30) }
+    // Add more timetable data as needed
+  ];
+  return (
+    <div className=''>
+      <div className='row justify-content-around mb-4'>
+        <div className='col'>
+          <Card title='Students' icon={student} />
+        </div>
+        <div className='col'>
+          <Card title='Teachers' col-md- icon={teacher} />
+        </div>
+        <div className='col'>
+          <Card title='Parents' icon={parents} />
+        </div>
+      </div>
+      <div className='row justify-content-center'>
+        <div className='col-12 col-md-5 mb-4 Charts'>
+          <DoughnutChart maleCount={maleCount} femaleCount={femaleCount} />
+        </div>
+        <div className='col-12 col-md-6 mb-4 ms-3 Charts'>
+          <BarChart data={chartData.courses} />
+        </div>
+      </div>
+      <div className='col-12 mx-auto mb-4 Charts'>
+        <TimetableScheduler data={timetableData} />
+      </div>
+    </div>
+  );
+  
+};
+export default DashboardDirecteur;
+
