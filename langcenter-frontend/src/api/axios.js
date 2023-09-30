4bfd7f573d9a90042b8ce66dd1de61a6c68bc552
Lachhab1 <Mohamed172lachhab@gmail.com>
2023-09-30 15:01:13 +0100

    changes to production

--- langcenter-frontend/src/api/axios.js
+++ langcenter-frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosCleint= axios.create({
-    baseURL: "https://english-castle.azurewebsites.net:8000",
+    baseURL: "http://localhost:8000",
     headers: {
         "Accept": "application/json",
         "Content-Type": "application/json",
